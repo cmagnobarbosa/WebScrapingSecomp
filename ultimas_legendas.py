a143f2030918efb0e99212a423a3e79815ee56d6
exterminus <cmagnobarbosa@gmail.com>
2018-08-29 14:16:29 -0300

    crawler simples que retorna as últimas legendas postadas no legendas.tv

---
+++ ultimas_legendas.py
@@ -0,0 +1,41 @@
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.firefox.options import Options
+from bs4 import BeautifulSoup
+"""
+Descricao:
+Crawler Simples para listar as últimas legendas do legendas.tv
+
+MiniCurso: WebScrapping
+Secomp 2018
+
+Carlos Magno
+Lucas Félix
+UFSJ
+
+Requisitos:
+* python3
+* requests, BeautifulSoup, selenium, geckodriver
+
+"""
+# Endereço do site a ser "crawleado".
+site_alvo="http://legendas.tv/legendas/-/d/1"
+
+
+#selenium
+options = Options()
+options.set_headless(headless=True)
+#webdriver
+navegador = webdriver.Firefox(firefox_options=options)
+navegador.get(site_alvo)
+#Retorna uma lista de elementos de acordo com o nome da classe.
+destaques = navegador.find_elements_by_class_name('destaque')
+
+for destaque in destaques:
+    #extrai o html
+    html= destaque.get_attribute('innerHTML')
+    parser=BeautifulSoup(html,"lxml")
+    print("Legenda:",parser.find("a").text)
+    print("Detalhes:",parser.find("p",{"class":"data"}).text)
+    print("-----------")
