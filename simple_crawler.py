21c2c20f7ba9a2bde321981ed336983e0413befa
exterminus <cmagnobarbosa@gmail.com>
2018-09-10 23:50:54 -0300

    apresentacao completa

--- simple_crawler.py
+++ simple_crawler.py
@@ -11,26 +11,4 @@ Apresenta os recursos basicos do BeautifulSoup
 site= requests.get("http://example.com")
 #retorno do codigo 200, indicando que o conteúdo foi coletado.
 print("Status:",site.status_code)
-print("Encoding:",site.encoding)
-print("Cabeçalho",site.headers)
-soup=BeautifulSoup(site.content,"html.parser")
-#acessar a tag
-print(soup.title)
-#acessa o conteúdo da tag html
-print(soup.html)
-
-print("----")
-print(soup.div.h1.text)
-
-#pesquisa usando os métodos
-print("First",soup.find("a"))
-for i in soup.findAll("a"):
-    print(i['href'])
-
-#Fazendo um select
-print(soup.select("p"))
-#print(soup.link('href'))
-
-#extrai o texto da pagina
-# print("Extrai o texto da pagina")
-# print(soup.get_text())
+print("Encoding:",site.encogit
